ad9409b6b63a8518a5cff66c1fb301f1757f1ca4
kenjetaev.ermek@hotmail.com <seni901495>
2014-12-15 00:10:40 +0600

    Сделать вывод имени пользователя, вместо формы авторизации, если он авторизован Если пользователь авторизовался с сайта, а не с форума, то делать редирект на сайт.

--- www/application/views/front/templates/header.php
+++ www/application/views/front/templates/header.php
@@ -24,30 +24,41 @@
                     ?>
                 </ul>
                 <div class="login-block">
-                    <form action="/forum/ucp.php?mode=login&sid=fh4gpf32rtsb0y513wnaowenfp0q2394">
+                    <?php if (!$this->session->userdata('phpbb_username') && !$this->session->userdata('phpbb_userpassword')): ?>
+                    <form method="get" action="/forumlogin/login">
                         <label><a href="/forum/ucp.php?mode=login">Авторизация</a></label>
                         <input type="text" name="username" placeholder="имя или e-mail" />
                         <input type="password" name="password" placeholder="пароль" />
-                        <input type="hidden" name="redirect" value="/forum/" />
+                        <input type="hidden" name="redirect" value="/" />
                         <input type="hidden" name="login" value="external" />
                         <input type="submit" value="Войти"/>
                     </form>
-                    <a href="/forum/ucp.php?mode=register">Регистрация</a>
-                    <form method="post" action="/forum/ucp.php?mode=register&sid=9tq6r0t2gd34g38a68f0t01f3gf68a54">
-                        <label>Регистрация</label>
-                        <input type="text" name="username" placeholder="логин" />
-                        <input type="text" name="email" placeholder="e-mail" />
-                        <input type="password" name="new_password" placeholder="пароль" />
-                        <input type="password" name="password_confirm" placeholder="подтверждение пароля" />
-                        <input type="hidden" name="redirect" value="/forum/" />
-                        <input type="hidden" name="login" value="external" />
-                        <input type="hidden" name="from_external" value="external" />
-                        <input type="hidden" name="tz" value="Asia/Bishkek" />
-                        <input type="hidden" name="lang" value="ru" />
-                        <input type="hidden" name="creation_time" value="<?=time()?>" />
-                        <input type="hidden" name="submit" value="1" />
-                        <input type="submit" value="Войти"/>
-                    </form>
+                        <a href="/forum/ucp.php?mode=register">Регистрация</a>
+
+                        <form method="post" action="/forum/ucp.php?mode=register&sid=9tq6r0t2gd34g38a68f0t01f3gf68a54">
+                            <label>Регистрация</label>
+                            <input type="text" name="username" placeholder="логин" />
+                            <input type="text" name="email" placeholder="e-mail" />
+                            <input type="password" name="new_password" placeholder="пароль" />
+                            <input type="password" name="password_confirm" placeholder="подтверждение пароля" />
+                            <input type="hidden" name="redirect" value="/forum/" />
+                            <input type="hidden" name="login" value="external" />
+                            <input type="hidden" name="from_external" value="external" />
+                            <input type="hidden" name="tz" value="Asia/Bishkek" />
+                            <input type="hidden" name="lang" value="ru" />
+                            <input type="hidden" name="creation_time" value="<?=time()?>" />
+                            <input type="hidden" name="submit" value="1" />
+                            <input type="submit" value="Войти"/>
+                        </form>
+                    <?php else:?>
+                        <p>Добро пожаловать, <?=$this->session->userdata('phpbb_username')?>! <a href="/forumlogin/login1">Перейти в форум</a></p>
+                        <p><a href="forumlogin/logout">Выход</a></p>
+                    <?php endif;?>
+                    <script type="text/javascript">
+
+                    </script>
+
+
 
                 </div>
             </div>
